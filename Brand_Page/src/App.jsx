06a6975b92764de9a136b3a6f903a805e3bff36f
student-gauravrawat <student-rawatgaurav23092004@gmail.com>
2024-08-08 17:47:35 +0530

    Navigation

---
+++ Brand_Page/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navigation from './component/Navigation'
+
+function App() {
+
+  return (
+    <>
+      <Navigation/>
+    </>
+  )
+}
+
+export default App
---
+++ Brand_Page/src/component/Navigation.jsx
@@ -0,0 +1,24 @@
+
+
+export default function Navigation () {
+    return(
+        <>
+            <nav className="container">
+             <div className="logo">
+                <img src="/image/brand_logo.png" alt="img" />
+             </div>
+
+
+              <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+              </ul>
+
+              <button>Login</button>
+
+            </nav>
+        </>
+    )
+}
